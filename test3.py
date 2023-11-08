b6cd7bc46946cb1c971f871094b9575639810b15
IfeAgboola <139165784+IfeAgboola@users.noreply.github.com>
2023-11-08 11:46:51 -0700

    Add files via upload

--- test3.py
+++ test3.py
@@ -1,7 +1,7 @@
 num1 = int(input("Enter a number:"))
 
-num2 =   int(input("Enter another number:"))
+num2 = int(input("Enter another number:"))
 
-result = num1+num2
+result = num1 + num2
 
-print(result);
+print(result)
